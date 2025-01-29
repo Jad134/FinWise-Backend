5ef428e9ddb037e896084f3f78d7dc6e8905975d
unknown <jadelnader99@gmail.com>
2025-01-29 19:02:26 +0100

    start user app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    mobile_number = models.CharField(max_length=15, unique=True)
+    date_of_birth = models.DateField()
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    REQUIRED_FIELDS = [ 'mobile_number', 'date_of_birth']
+
+    def __str__(self):
+        return self.username
