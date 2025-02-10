9b2d38096766993b90a0d8ff64e8445e24931a01
unknown <jadelnader99@gmail.com>
2025-02-10 15:28:04 +0100

    clean code authentication

--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from accounts.models import CustomUser
 
 
 class RegisterView(APIView):
@@ -31,25 +32,36 @@ class CustomAuthToken(APIView):
         username = request.data.get("username")
         password = request.data.get("password")
 
-        if not username or not password:
-            return Response(
-                {"error": "Benutzername und Passwort erforderlich"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
 
-        user = authenticate(username=username, password=password)
-
+        error_response = self.check_credentials(username, password)
+        if error_response:
+            return error_response
+        
+        user = self.check_user_password(username, password)
         if user is None:
-            if not User.objects.filter(username=username).exists():
-                return Response(
-                    {"error": "Benutzer existiert nicht"},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
             return Response(
-                {"error": "Falsches Passwort"},
+                {"error": "Falsches Passwort oder Benutzer existiert nicht"},
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
         token, created = Token.objects.get_or_create(user=user)
 
         return Response({"token": token.key}, status=status.HTTP_200_OK)
+    
+    def check_credentials(self, username, password):
+        """Überprüft, ob Benutzername und Passwort vorhanden sind."""
+        if not username or not password:
+            return Response(
+                {"error": "Benutzername und Passwort erforderlich"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        return None    
+
+    def check_user_password(self, username, password):
+        """Überprüft Benutzerexistenz und Passwort und gibt den Benutzer zurück."""
+        user = authenticate(username=username, password=password)
+        if user is None:
+            
+            if not CustomUser.objects.filter(username=username).exists():
+                return None 
+        return user
