c67dfa478e00f76f018aff03131b088846355b71
unknown <jadelnader99@gmail.com>
2025-03-06 19:06:29 +0100

    expenses view admin panel with amount

--- finance_app/admin.py
+++ finance_app/admin.py
@@ -3,12 +3,10 @@ from .models import Expense
 from django.utils.html import format_html
 from django.contrib.auth.models import User
 
-# Zeigt den Benutzernamen an und macht ihn klickbar, um zur Detailansicht des Benutzers zu gelangen
 class ExpenseAdmin(admin.ModelAdmin):
-    list_display = ('user', 'category')  # Nur Benutzername und Kategoriebetrag anzeigen
+    list_display = ('user', 'category', 'amount')  
     list_filter = ('user', 'category')
     search_fields = ('category', 'user__username')
-
 
 
     def changelist_view(self, request, extra_context=None):
